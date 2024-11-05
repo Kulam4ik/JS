6bbdbfc972d347286913a2f8eb820592fc2d9640
Kulam4ik <reimonzozuk123@gmail.com>
2024-11-05 17:48:45 +0200

    first commit

---
+++ number.js
@@ -0,0 +1,41 @@
+const age = 14
+console.log(age);
+
+const name ="Maksim"
+console.log(name);
+
+ const isStudent = true
+
+console.log(isStudent);
+
+ const myString ="Спочатку мрії здаються неможливими, потім неправдоподібними, а потім неминучими."
+ console.log(myString);
+
+ let myNumber = 52
+
+ myNumber = myNumber + 10  
+//  myNumber += 10
+ console.log(myNumber);
+
+ let myNull = null;
+ console.log(myNull);
+
+  const myName = prompt("Введіть Ім'я")
+  console.log(myName);
+  alert(`${myName} привіт`)
+  alert("Привіт" + " " +   myName )
+  
+
+ const data = confirm("Зробив своє д/з?")
+ if(data) {
+   alert ("Дякую за підтвердження!");
+ }else {
+   alert ("Дію відмінено!")
+ }
+
+ const data2 = confirm("Це небезпечно ти впевнений?")
+ if(data2) {
+    alert("Дякую за підтвердження!")
+ }else {
+    alert("Дію відмінено!")
+ }
